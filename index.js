3f1c6cda5cb6366644e9cd51400b92e31fa85dc7
Aung Myat Phone <132052171+aungmp@users.noreply.github.com>
2023-12-16 01:13:03 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,54 @@
+var drums = document.querySelectorAll(".drum");
+
+
+ 
+for (var i = 0; i < drums.length; i++) {
+   drums[i].addEventListener("click", function(){
+      this.classList.add('color');
+       var dr = this.innerText;
+
+      
+      switch (dr) {
+         case "w":
+            
+            var w = new Audio("sounds/tom-1.mp3");
+            w.play();
+             
+            break;
+         case "a":
+            var a = new Audio("sounds/tom-2.mp3");
+            a.play();
+            break;
+         case "s":
+            var s = new Audio("sounds/tom-3.mp3");
+            s.play();
+            break;
+         case "d":
+            var d = new Audio("sounds/tom-3.mp3");
+            d.play();
+            break;
+         case "j":
+            var j=new Audio("sounds/snare.mp3");
+            j.play();
+            break;
+         case "k":
+            var k=new Audio("sounds/crash.mp3")
+            k.play();
+            break;
+         case "l":
+            var l=new Audio("sounds/kick-bass.mp3");
+            l.play();
+            break;
+            default:
+
+         
+
+      }
+   }
+
+     
+   
+   );
+}
+
+
